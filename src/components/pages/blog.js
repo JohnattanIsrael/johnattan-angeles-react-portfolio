0b5c8354f1bd3dbb3cd6afd04f166c8b32a41703
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-09-16 19:29:44 -0500

    fixing memory lick hidden problem

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -18,11 +18,11 @@ class Blog extends Component {
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfinitaScroll();
+        this.onScroll = this.onScroll.bind(this);
+        window.addEventListener('scroll', this.onScroll, false);
     }
 
-    activateInfinitaScroll() {
-        window.onscroll = () => {
+    onScroll() {
             if (
                 this.state.isLoading || 
                 this.state.blogItems.length === this.state.totalCount
@@ -35,7 +35,7 @@ class Blog extends Component {
             ) {
                 this.getBlogItems();     
             } 
-        };
+        
     }
 
     getBlogItems() {
@@ -46,7 +46,7 @@ class Blog extends Component {
         axios.get(`https://johnattan.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
             { withCredentials: true }
         ).then(response => {
-            // console.log('getting', response.data);
+            console.log('getting', response.data);
             this.setState({
                 blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
                 totalCount: response.data.meta.total_records,
@@ -59,6 +59,10 @@ class Blog extends Component {
 
     componentWillMount() {
         this.getBlogItems();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, false);
     }
 
     render() {
